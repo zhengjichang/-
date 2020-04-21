289dd7c1dbd9e3a3b3580791c8c636ffbe82acc2
unknown <1491725654@qq.com>
2020-04-21 09:34:53 +0800

    vue

---
+++ a.vue
@@ -0,0 +1,33 @@
+<!--  -->
+<template>
+<div id=''>
+
+</div>
+</template>
+
+<script>
+export default {
+name:'',
+components:{
+
+},
+data:function() {
+return {
+
+}
+},
+//生命周期 - 创建完成（访问当前this实例）
+created() {
+
+},
+//生命周期 - 挂载完成（访问DOM元素）
+mounted() {
+// 网络请求/数据库查询/延时操作等
+
+}
+}
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+
+</style>
